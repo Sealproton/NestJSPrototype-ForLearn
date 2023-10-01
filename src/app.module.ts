3b37c15524889e22ba573dd51651136abf518db6
sealproton <sealproton@gmail.com>
2023-10-01 18:23:44 +0700

    feat: add Report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
+import { Report } from './report/report.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
     UserModule,
