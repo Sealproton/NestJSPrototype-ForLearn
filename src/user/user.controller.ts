0840e94f967c63f801e785316e8c1c63c29ac91f
sealproton <sealproton@gmail.com>
2023-10-08 17:00:10 +0700

    feat: add Get Delete Remoce method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,42 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUsetDto } from './dtos/create-user.dto';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserService } from './user.service';
 @Controller('auth')
 export class UserController {
   constructor(private userService: UserService) {}
   @Post('/signup')
-  createUser(@Body() body: CreateUsetDto) {
+  createUser(@Body() body: CreateUserDto) {
     this.userService.create(body.email, body.password);
+  }
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(Number(id));
+    if (!user) {
+      throw new NotFoundException('user Not Found');
+    }
+    return user;
+  }
+  @Get('/')
+  findAllUser(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.remove(Number(id));
+  }
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(Number(id), body);
   }
 }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterRemove,
+  AfterUpdate,
+} from 'typeorm';
 //Its decorator to help TypeORm to understand some dif properties we going to add to entity
 @Entity()
 export class User {
@@ -7,7 +13,17 @@ export class User {
 
   @Column()
   email: string;
-  
+
   @Column()
   password: string;
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Update user at id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Remove user at id', this.id);
+  }
 }
