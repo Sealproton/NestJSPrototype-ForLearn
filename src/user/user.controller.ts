54690ab930893c82623585e01bcb402b5ad987c0
sealproton <sealproton@gmail.com>
2023-10-08 15:54:50 +0700

    feat: add entity injection for UserService

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUsetDto } from './dtos/create-user.dto';
+import { UserService } from './user.service';
 @Controller('auth')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUsetDto) {
-    console.log(body);
+    this.userService.create(body.email, body.password);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 @Injectable()
-export class UserService {}
+export class UserService {
+  //1
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+}
+
+// 1
+// เราตเองการ repo เป็น instance of TypeORM Repository ที่จัดการกับ instance of  User entity
