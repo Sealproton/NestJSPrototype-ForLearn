2b75c79c6f76365777705817b8f72b5c62477543
sealproton <sealproton@gmail.com>
2023-10-09 13:56:29 +0700

    feat: set Serialize interceptor to controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,6 +14,7 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserService } from './user.service';
+import { SerializeInterceptor } from 'src/Interceptors/serialize.interceptor';
 @Controller('auth')
 export class UserController {
   constructor(private userService: UserService) {}
@@ -21,7 +22,7 @@ export class UserController {
   createUser(@Body() body: CreateUserDto) {
     this.userService.create(body.email, body.password);
   }
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.userService.findOne(Number(id));
