48f1cd2312eefbb6e7abc56432ece8a9f5693284
sealproton <sealproton@gmail.com>
2023-10-13 12:03:51 +0700

    test(AuthService): refactor

--- src/user/auth.service.spec.ts
+++ src/user/auth.service.spec.ts
@@ -5,15 +5,22 @@ import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { User } from './user.entity';
 
 describe('AuthService', () => {
+  let users: User[] = [];
   let service: AuthService;
   let fakeUserService: Partial<UserService>;
   beforeEach(async () => {
     //Create fake coppy of userService
     fakeUserService = {
       //Promise.result คือคำสั่งให้สร้าง promise แล้ว resolve ให้ค่าในวงเล็บ
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = { id: users.length, email, password } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
     const module = await Test.createTestingModule({
       providers: [
@@ -35,29 +42,25 @@ describe('AuthService', () => {
     expect(hash).toBeDefined();
   });
   it('throws an error if user signs up with email that is in use', async () => {
-    //reassign find in case found user
-    fakeUserService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await service.signUp('asdf@asdf.com', 'asdf');
     await expect(service.signUp('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
     );
   });
   it('throws if signin is called with an unused email', async () => {
     await expect(
-      service.signIn('1243124124@gmail.com', 'laskdjf'),
+      service.signIn('asdflkj@asdlfkj.com', 'passdflkj'),
     ).rejects.toThrow(NotFoundException);
   });
   it('throws if an invalid password is provided', async () => {
-    fakeUserService.find = () =>
-      Promise.resolve([
-        {
-          email: 'asdf@asdf.com',
-          password:
-            'b5671bb3ff32b8fc.f3722dacc977b923a72e8662653d3dd4511d1ed2d86ad7bb02fe2361599157b1',
-        } as User,
-      ]);
-    await expect(service.signIn('asdf@asdf.com', 'Zlaskdjf')).rejects.toThrow(
+    await service.signUp('test@asdf.com', 'Zlaskdjf');
+    await expect(service.signIn('test@asdf.com', 'Zlaskdjf1')).rejects.toThrow(
       BadRequestException,
     );
+  });
+  it('return user id login successfully', async () => {
+    await service.signUp('success@gmail.com', '1234');
+    const signin = await service.signIn('success@gmail.com', '1234');
+    expect(signin).toBeDefined();
   });
 });
